6dd0658094a709e039e5e7f952d4db158bbaaee0
Fan Cui <cuifaneo@gmail.com>
2025-08-14 16:39:26 +0800

    migrate to remote server

--- compile.sh
+++ compile.sh
@@ -1,16 +1,11 @@
-#!/bin/zsh
-
-source ~/.zshrc
+#!/bin/bash
 
 rm -rf build
-
-conda activate faiss_arm64
 
 cmake -B build -DFAISS_ENABLE_GPU=OFF \
   -DBUILD_TESTING=OFF \
   -DFAISS_ENABLE_C_API=ON \
   -DBUILD_SHARED_LIBS=ON \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
   -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-undefined,dynamic_lookup" \
   -DCMAKE_EXE_LINKER_FLAGS="-Wl,-undefined,dynamic_lookup" \
   .
